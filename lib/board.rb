a109b98bf6d4318f7f60af7af7afbea727f717ea
Craig J. Bass <craig@madetech.co.uk>
2017-10-08 15:10:13 +0100

    Extract more board related methods

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,7 @@
 class Board
-  def initialize
+  def initialize(size:)
     @array = empty_board
+    @size = size
   end
 
   def empty_board
@@ -23,6 +24,29 @@ class Board
     @array
       .each_index
       .select { |i| @array[i] == 'X' }
+  end
+
+  def occupied_xy_coordinates
+    occupied_linear_positions.map do |linear_position|
+      xy_coordinate_for(linear_position)
+    end
+  end
+
+  def grid_coordinates
+    (1...@size + 1)
+      .to_a
+      .repeated_permutation(2)
+      .to_a
+  end
+
+  def xy_coordinate_for(linear_position)
+    [linear_position % @size + 1, linear_position / @size + 1]
+  end
+
+  def linear_position_for(x, y)
+    zero_indexed_y = y - 1
+    zero_indexed_x = x - 1
+    (zero_indexed_y * @size) + zero_indexed_x
   end
 
   def to_a
--- lib/game.rb
+++ lib/game.rb
@@ -1,26 +1,24 @@
 require 'board'
 
 class Game
-  GRID_SIZE = 3
-
   def start(presenter)
     @presenter = presenter
-    @board = Board.new
+    @board = Board.new(size: 3)
 
     present
   end
 
   def place(x, y)
-    position = linear_position_for(x, y)
+    position = @board.linear_position_for(x, y)
     @board[position] = 'X'
 
     present
   end
 
   def available_coordinates
-    occupied_coordinates = occupied_xy_coordinates
+    occupied_coordinates = @board.occupied_xy_coordinates
 
-    grid_coordinates.reject { |c| occupied_coordinates.include?(c) }
+    @board.grid_coordinates.reject { |c| occupied_coordinates.include?(c) }
   end
 
   private
@@ -28,28 +26,5 @@ class Game
   def present
     @presenter.present(@board.to_a)
     nil
-  end
-
-  def occupied_xy_coordinates
-    @board.occupied_linear_positions.map do |linear_position|
-      xy_coordinate_for(linear_position)
-    end
-  end
-
-  def grid_coordinates
-    (1...GRID_SIZE + 1)
-      .to_a
-      .repeated_permutation(2)
-      .to_a
-  end
-
-  def xy_coordinate_for(linear_position)
-    [linear_position%GRID_SIZE+1, linear_position/GRID_SIZE+1]
-  end
-
-  def linear_position_for(x, y)
-    zero_indexed_y = y - 1
-    zero_indexed_x = x - 1
-    (zero_indexed_y * GRID_SIZE) + zero_indexed_x
   end
 end
